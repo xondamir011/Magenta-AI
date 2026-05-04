f461c383b527f6182b63acadc4ed3330b0dba153
xondamir011 <xondamirmadaliyev79@gmail.com>
2026-05-04 19:48:14 +0500

    remove

--- src/data/users.js
+++ src/data/users.js
@@ -15,12 +15,108 @@ export const users = [
     salary: 800,
     job: "designer"
   },
-   {
+  {
     username: "@rayxonmi",
     age: 22,
     city: "Samarkand",
     height: 158,
     salary: 800,
     job: "designer"
+  },
+  {
+    username: "@madina_uz",
+    age: 20,
+    city: "Tashkent",
+    height: 165,
+    salary: 500,
+    job: "student"
+  },
+  {
+    username: "@gulnoza_dev",
+    age: 24,
+    city: "Tashkent",
+    height: 168,
+    salary: 1500,
+    job: "frontend"
+  },
+  {
+    username: "@aziza_style",
+    age: 21,
+    city: "Bukhara",
+    height: 160,
+    salary: 700,
+    job: "stylist"
+  },
+  {
+    username: "@shaxnoza_fit",
+    age: 23,
+    city: "Tashkent",
+    height: 170,
+    salary: 900,
+    job: "trainer"
+  },
+  {
+    username: "@nilufar_art",
+    age: 26,
+    city: "Samarkand",
+    height: 162,
+    salary: 1100,
+    job: "artist"
+  },
+  {
+    username: "@laylo_travel",
+    age: 25,
+    city: "Khiva",
+    height: 167,
+    salary: 1200,
+    job: "bloger"
+  },
+  {
+    username: "@sabina_makeup",
+    age: 22,
+    city: "Tashkent",
+    height: 159,
+    salary: 1000,
+    job: "makeup"
+  },
+  {
+    username: "@dilnoza_hr",
+    age: 27,
+    city: "Tashkent",
+    height: 164,
+    salary: 1400,
+    job: "hr"
+  },
+  {
+    username: "@zarina_law",
+    age: 28,
+    city: "Samarkand",
+    height: 169,
+    salary: 2000,
+    job: "lawyer"
+  },
+  {
+    username: "@noza_cook",
+    age: 30,
+    city: "Bukhara",
+    height: 161,
+    salary: 600,
+    job: "chef"
+  },
+  {
+    username: "@lola_teacher",
+    age: 29,
+    city: "Tashkent",
+    height: 166,
+    salary: 800,
+    job: "teacher"
+  },
+  {
+    username: "@farida_shop",
+    age: 23,
+    city: "Andijan",
+    height: 163,
+    salary: 900,
+    job: "seller"
   }
 ];
